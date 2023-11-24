e999c8ec75025e2252b7cc177cfd92178b5874ea
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2023-11-24 21:40:20 +0200

    added date to article link

--- src/articles/article-files.js
+++ src/articles/article-files.js
@@ -1,1 +1,3 @@
-export const articleFiles = [{ id: "01", name: "#1 忏悔就是改过自新" }]; // Manual list the article IDs
+export const articleFiles = [
+    { id: "01", name: "#1 忏悔就是改过自新(24.11.23)" },
+]; // Manual list the article IDs
